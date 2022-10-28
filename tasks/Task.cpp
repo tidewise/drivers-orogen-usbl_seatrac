0c07af3ea61253e0ebaae150e1ce04727541c3e0
Debora <debora.tahara.valverde@gmail.com>
2022-10-28 19:02:19 -0300

    fix: Fix timestamp

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -19,7 +19,7 @@ static RigidBodyState convertToRBS(PingStatus const& data)
 {
     RigidBodyState rbs;
 
-    rbs.time = base::Time::now();
+    rbs.time = data.timestamp;
     rbs.position = Eigen::Vector3d(data.response.acoustic_fix.position.north,
         -data.response.acoustic_fix.position.east,
         -data.response.acoustic_fix.position.depth);
@@ -62,6 +62,7 @@ bool Task::startHook()
 void Task::updateHook()
 {
     PingStatus status = mDriver->Ping(mDestinationId, mMsgType);
+    status.timestamp = base::Time::now();
     _ping_status.write(status);
     if (status.flag == 1) {
         auto rbs = convertToRBS(status);
