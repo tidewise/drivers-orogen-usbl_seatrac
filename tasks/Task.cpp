ff346e692be78f0d7cd8cb5e322306214026cc78
Debora <debora.tahara.valverde@gmail.com>
2023-03-22 11:11:59 -0300

    fix: Fix convertToQuaternion

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -20,11 +20,11 @@ Task::~Task()
 static Eigen::Quaterniond convertToQuaterniond(PingStatus const& data)
 {
     Eigen::Quaterniond attitude = Eigen::Quaterniond(
-        Eigen::AngleAxisd(data.response.acoustic_fix.attitude_roll / 10. / 180.0 * M_PI,
+        Eigen::AngleAxisd(data.response.acoustic_fix.attitude_yaw / 10. / 180.0 * M_PI,
             Eigen::Vector3d::UnitX()) *
         Eigen::AngleAxisd(data.response.acoustic_fix.attitude_pitch / 10. / 180.0 * M_PI,
             Eigen::Vector3d::UnitY()) *
-        Eigen::AngleAxisd(data.response.acoustic_fix.attitude_yaw / 10. / 180.0 * M_PI,
+        Eigen::AngleAxisd(data.response.acoustic_fix.attitude_roll / 10. / 180.0 * M_PI,
             Eigen::Vector3d::UnitZ()));
     return attitude;
 }
@@ -78,32 +78,34 @@ bool Task::configureHook()
     mDriver = move(driver);
     guard.commit();
 
-    // Set initial settings
-    // protocol::Settings new_settings = mDriver->getSettingsProtocol();
-
-    // uint8_t status_flags = _status_mode.get();
-    // uint8_t enviromental_flags = _auto_vos.get() | (_auto_pressure_ofs.get() << 1);
-    // uint8_t ahrs_flags = _auto_cal_mag.get();
-    // uint8_t xcvr_flags = (_usbl_use_ahrs.get()) | (_xcvr_posflt_enable.get() << 1) |
-    //                      (_xcvr_tx_msgctrl.get() << 3) | (_xcvr_usbl_msgs.get() << 5) |
-    //                      (_xcvr_fix_msgs.get() << 6) | (_xcvr_diag_msgs.get() << 7);
-
-    // new_settings.status_flags = status_flags;
-    // new_settings.enviromental_flags = enviromental_flags;
-    // new_settings.ahrs_flags = ahrs_flags;
-    // new_settings.xcvr_flags = xcvr_flags;
-    // new_settings.xcvr_range_tmo = static_cast<uint16_t>(_xcvr_range_tmo.get());
-    // new_settings.xcvr_resp_time =
-    //     static_cast<uint16_t>(_xcvr_resp_time.get().toMilliseconds());
-    // new_settings.xcvr_posflt_tmo =
-    //     static_cast<uint16_t>(_xcvr_posflt_tmo.get().toSeconds());
-    // new_settings.xcvr_beacon_id = _xcvr_beacon_id.get();
-    // mDriver->setSettingsProtocol(new_settings);
+    
     return true;
 }
 
 bool Task::startHook()
 {
+    // Set initial settings
+    protocol::Settings new_settings = mDriver->getSettingsProtocol();
+
+    uint8_t status_flags = _status_mode.get();
+    uint8_t enviromental_flags = _auto_vos.get() | (_auto_pressure_ofs.get() << 1);
+    uint8_t ahrs_flags = _auto_cal_mag.get();
+    uint8_t xcvr_flags = (_usbl_use_ahrs.get()) | (_xcvr_posflt_enable.get() << 1) |
+                         (_xcvr_tx_msgctrl.get() << 3) | (_xcvr_usbl_msgs.get() << 5) |
+                         (_xcvr_fix_msgs.get() << 6) | (_xcvr_diag_msgs.get() << 7);
+
+    new_settings.status_flags = status_flags;
+    new_settings.enviromental_flags = enviromental_flags;
+    new_settings.ahrs_flags = ahrs_flags;
+    new_settings.xcvr_flags = xcvr_flags;
+    new_settings.xcvr_range_tmo = static_cast<uint16_t>(_xcvr_range_tmo.get());
+    new_settings.xcvr_resp_time =
+        static_cast<uint16_t>(_xcvr_resp_time.get().toMilliseconds());
+    new_settings.xcvr_posflt_tmo =
+        static_cast<uint16_t>(_xcvr_posflt_tmo.get().toSeconds());
+    new_settings.xcvr_beacon_id = _xcvr_beacon_id.get();
+    mDriver->setSettingsProtocol(new_settings);
+    
     if (!TaskBase::startHook()) {
         return false;
     }
