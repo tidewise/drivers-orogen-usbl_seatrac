4740bfdc19471eb12e440e6d8d14a26aad2aadcd
Debora <debora.tahara.valverde@gmail.com>
2023-03-23 16:43:45 -0300

    fix[rubocop] : Formating

--- test/task_test.rb
+++ test/task_test.rb
@@ -55,15 +55,16 @@ describe OroGen.usbl_seatrac.Task do
         end
 
         it "interprets a ping response message from the device" do
-            packet1 = raw_packet_from_s("$103FA4E50F0000000000285D09010D00000001000000133C2" \
-                "406EDFFF9F80EFF090100005EF9FE1AFFE1FEC400F3005301EDFF0200EA009B0551FDC8FB1" \
-                "6000000040070C8B6C018E2D24055687443A410BA4487451DC41C019CC40000B0410000000" \
-                "0000080408C00\r\n$")
+            packet1 = raw_packet_from_s("$103FA4E50F0000000000285D09010D00000001000000" \
+                "133C2406EDFFF9F80EFF090100005EF9FE1AFFE1FEC400F3005301EDFF0200EA009B0" \
+                "551FDC8FB16000000040070C8B6C018E2D24055687443A410BA4487451DC41C019CC4" \
+                "0000B04100000000000080408C00\r\n$")
             packet2 = raw_packet_from_s("$4000028015\r\n")
-            packet3 = raw_packet_from_s("$42020F07020101010101010101010101010101010101010" \
-                "101010101010101010101010101010101010101010101010101B0\r\n")
+            packet3 = raw_packet_from_s("$42020F07020101010101010101010101010101010101" \
+                "010101010101010101010101010101010101010101010101010101B0\r\n")
             response =
-                expect_execution { syskit_write @raw_io.out_port, packet1, packet2, packet3 }
+                expect_execution \
+                    { syskit_write @raw_io.out_port, packet1, packet2, packet3 }
                 .to do
                     [
                         have_one_new_sample(task.local_pose_port),
@@ -79,9 +80,15 @@ describe OroGen.usbl_seatrac.Task do
             assert_equal(0, response[1].position.x)
             assert_equal(0, response[1].position.y)
             assert_equal(0.1, response[1].position.z)
-            assert_in_delta((-179.9 * (3.14159265 / 180)), response[1].orientation.roll, 0.1)
-            assert_in_delta((-1.9 * (3.14159265 / 180)), response[1].orientation.pitch, 0.1)
-            assert_in_delta((157.2 * (3.14159265 / 180)), response[1].orientation.yaw, 0.1)
+            assert_in_delta(
+                (-179.9 * (3.14159265 / 180)), response[1].orientation.roll, 0.1
+            )
+            assert_in_delta(
+                (-1.9 * (3.14159265 / 180)), response[1].orientation.pitch, 0.1
+            )
+            assert_in_delta(
+                (157.2 * (3.14159265 / 180)), response[1].orientation.yaw, 0.1
+            )
 
             assert_equal(257, response[2].response.acoustic_fix.position.north)
             assert_equal(257, response[2].response.acoustic_fix.position.east)
@@ -92,45 +99,44 @@ describe OroGen.usbl_seatrac.Task do
         end
 
         it "interprets a ping error message from the device" do
-            packet1 = raw_packet_from_s("$1001B200000000000000010001000100000001000000010" \
-                "0B429\r\n$")
+            packet1 = raw_packet_from_s("$1001B200000000000000010001000100000001000000" \
+                "0100B429\r\n$")
             packet2 = raw_packet_from_s("$4000028015\r\n")
             packet3 = raw_packet_from_s("$43340266D5\r\n")
-            response =
-                expect_execution { syskit_write @raw_io.out_port, packet1, packet2, packet3 }
+            expect_execution { syskit_write @raw_io.out_port, packet1, packet2, packet3 }
                 .to { have_no_new_sample task.local_pose_port, at_least_during: 0.5 }
         end
 
         it "stop if there is no status packet from the device" do
             packet1 = raw_packet_from_s("$4000028015\r\n")
-            packet2 = raw_packet_from_s("$42020F07020101010101010101010101010101010101010" \
-                "101010101010101010101010101010101010101010101010101B0\r\n")
-            response =
-                expect_execution { syskit_write @raw_io.out_port, packet1, packet2}
+            packet2 = raw_packet_from_s("$42020F07020101010101010101010101010101010101" \
+                "010101010101010101010101010101010101010101010101010101B0\r\n")
+            expect_execution { syskit_write @raw_io.out_port, packet1, packet2 }
                 .to { have_no_new_sample task.local_pose_port, at_least_during: 0.5 }
         end
     end
 
-    def usbl_configure_and_start(raw_io, task) # rubocop: disable Metrics/AbcSize
-        get_reply = raw_packet_from_s("$15023F0D0D000000000000FA01A8C00000FFFF0101A8C"\
-            "00101A8C0A41F014F0000005E012B3C00FCFEFFFEE4FEE900F800F600FF405C2EC1D4B358C2"\
-            "2B3C4543F2C5833FE27C7E3FB5257A3F777E8942AE22D140000000000000000000000000630"\
-            "F64000A00000000000000030A3C3DB9\r\n")
+    # rubocop: disable Metrics/AbcSize
+    def usbl_configure_and_start(raw_io, task)
+        get_reply = raw_packet_from_s("$15023F0D0D000000000000FA01A8C00000FFFF0101A8C0" \
+            "0101A8C0A41F014F0000005E012B3C00FCFEFFFEE4FEE900F800F600FF405C2EC1D4B358C" \
+            "22B3C4543F2C5833FE27C7E3FB5257A3F777E8942AE22D140000000000000000000000000" \
+            "630F64000A00000000000000030A3C3DB9\r\n")
         set_reply = raw_packet_from_s("$16000E60\r\n")
         # Prepare the sample you want to send
         writer = syskit_create_writer raw_io.out_port
         get_cmd = expect_execution.scheduler(true)
-            .join_all_waiting_work(false)
-            .to { have_one_new_sample task.io_raw_out_port }
+                                  .join_all_waiting_work(false)
+                                  .to { have_one_new_sample task.io_raw_out_port }
         # check that get_cmd is actually a get command
         get_request = raw_packet_from_s("#15C1CF\r\n")
         assert_equal(get_request.data, get_cmd.data)
-
         set_cmd = expect_execution { writer.write get_reply }
-            .join_all_waiting_work(false)
-            .to { have_one_new_sample task.io_raw_out_port }
+                  .join_all_waiting_work(false)
+                  .to { have_one_new_sample task.io_raw_out_port }
         expect_execution { writer.write set_reply }
             .to { emit task.start_event }
         set_cmd
     end
+    # rubocop: enable Metrics/AbcSize
 end
